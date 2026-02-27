fa937ea3736d512d66ac020e2f164d652a7c0236
dududedaxiong <764840990@qq.com>
2026-02-27 16:28:03 +0000

    �� Auto sync ad blocking rules

--- js-ad-rules.js
+++ js-ad-rules.js
@@ -267,7 +267,8 @@
             "span[class^='vodContainer-list-item'].i(0)",
             ".van-tab.i(-3,-2)",
             "li.navbar-item.i(-3,-2)",
-            "a.abk-item.abk-banner"
+            "a.abk-item.abk-banner",
+            "div[class*='left']",
             
         ],
         "idsToHide": [
