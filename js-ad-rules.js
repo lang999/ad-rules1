528bf362380358eabc8c5dda6e694c2e7e95e692
dududedaxiong <764840990@qq.com>
2026-02-27 15:57:26 +0000

    �� Auto sync ad blocking rules

--- js-ad-rules.js
+++ js-ad-rules.js
@@ -263,8 +263,8 @@
             "span[class^='cysc']",
             "span[class^='tcjy']",
             "span[class^='vodContainer-list-item'].i(0)",
-         "*[class*='van-tab']:has(*:contains('脱衣'))",
-    "*[class*='van-tab']:has(*:contains('游戏'))"
+         "div[class*='van-tab']:has(*:contains('脱衣'))",
+    "div[class*='van-tab']:has(*:contains('游戏'))"
         ],
         "idsToHide": [
             "popup1",
